7a9959d4b1531be107ce3b4b3fc664d63a1ea714
mydearxym <mydearxym@qq.com>
2015-05-15 22:09:20 +0800

    add animate.css

---
+++ app/example/scripts/DashboardController.js
@@ -0,0 +1,37 @@
+angular
+  .module('example')
+  .controller('DashboardController', function($scope, supersonic) {
+  	var self = $scope;
+
+  	self.navbarTitle = "UI 测试哈"
+
+  	self.submit = function(){
+      supersonic.logger.warn("get submit: ")
+
+  		var view = new supersonic.ui.View("example#wave-test");
+  		supersonic.ui.layers.push(view);
+  	}
+  
+
+  });
+
+//init the animation staff
+
+$(function(){
+	var animationName = "animated rubberBand";
+	var animationEnd  = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	//todo: change it to touchstart
+	$("#dashboard-recommend").on("touchstart", function(){
+		var that = $(this);
+		that.addClass(animationName).one(animationEnd, function(){
+			that.removeClass(animationName)
+		})
+	})
+
+})
+
+
+
+
+
