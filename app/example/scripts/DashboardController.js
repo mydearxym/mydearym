be3ae5f75af91aa5b729bc590b39b4c9eb2f269f
mydearxym <mydearxym@qq.com>
2015-05-16 12:19:14 +0800

    add family modal works

--- app/example/scripts/DashboardController.js
+++ app/example/scripts/DashboardController.js
@@ -5,12 +5,11 @@ angular
 
   	self.navbarTitle = "UI 测试哈"
 
-  	self.submit = function(){
-      supersonic.logger.warn("get submit: ")
-
-  		var view = new supersonic.ui.View("example#wave-test");
-  		supersonic.ui.layers.push(view);
-  	}
+  	// self.submit = function(){
+   //    supersonic.logger.warn("get submit: ")
+  	// 	var view = new supersonic.ui.View("example#wave-test");
+  	// 	supersonic.ui.layers.push(view);
+  	// }
   
   	self.goToRecomment = function(){
   		$timeout(function(){
@@ -18,14 +17,33 @@ angular
   			supersonic.ui.layers.push(view);
   		})
   	}
+  	self.goToFamily = function(){
+  		var modalView = new supersonic.ui.View("example#family-config");
+  		var options = {
+  			animate: true
+  		}
+  		supersonic.ui.modal.show(modalView, options);
+  	}
+
 
   	jQuery("#mailMan").on("dashboard:swiper:goto", function(e, data){
       supersonic.logger.warn("dashboard:swiper:goto: " + data); // only + works
       console.log("@dashboard:swiper:goto: ", data)
   	})
 
-  });
+  })
 
+.controller("FamilyController", function($scope, supersonic, $timeout){
+	var self = $scope;
+
+	self.hideModal = function() {
+		console.log("hideModal");
+		var options = {
+			animate: true
+		}
+		supersonic.ui.modal.hide(options);
+	}
+})
 
 
 //init the animation staff
@@ -35,6 +53,13 @@ $(function(){
 
 	//todo: change it to touchstart
 	$("#dashboard-recommend").on("touchstart", function(){
+		var that = $(this);
+		that.addClass(animationName).one(animationEnd, function(){
+			that.removeClass(animationName)
+		})
+	})
+
+	$("#dashboard-family").on("touchstart", function(){
 		var that = $(this);
 		that.addClass(animationName).one(animationEnd, function(){
 			that.removeClass(animationName)
